d6424495ea198c048552cb52f08beeb5cd5e63c3
tittiong <103748355+tittiong@users.noreply.github.com>
2022-04-29 20:56:05 +1000

    Add files via upload

---
+++ KIT206-Assignment2/UnitList.cs
@@ -0,0 +1,23 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace KIT206_Assignment2
+{
+    public class UnitList
+    {
+        private List<Unit> unit;
+        public List<Unit> Units { get { return unit; } set { } }
+
+        private ObservableCollection<Unit> viewableUnit;
+        public ObservableCollection<Unit> VisibleUnits { get { return viewableUnit; } set { } }
+
+        public UnitList ()
+        {
+            unit = sqlConn.LoadAllUnits();
+            viewableUnit = new ObservableCollection<Unit>(unit);
+        }
+    }
+}
---
+++ KIT206-Assignment2/UnitView.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KIT206_Assignment2
+{
+    /// <summary>
+    /// Interaction logic for UnitView.xaml
+    /// </summary>
+    public partial class UnitView : Window
+    {
+        private UnitList unitList = new UnitList();
+
+        public UnitView()
+        {
+            InitializeComponent();
+
+            var list = unitList.VisibleUnits;
+            table.ItemsSource = list;
+        }
+
+        private void staffButton_Click(object sender, RoutedEventArgs e)
+        {
+            // With just one window we don't need to do too much here
+            // It's disabled by default so its obvious it shouldn't be pressed yet
+
+            // later on we'll probably have this button return us to the staff list
+            Console.WriteLine("Staff Button Pressed");
+        }
+
+        private void unitButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: add code to change/open unit view
+            Console.WriteLine("Unit Button Pressed");
+        }
+
+        private void classButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: add code to change/open class view
+            Console.WriteLine("Class Button Pressed");
+        }
+
+        private void consultationButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: add code to change/open consultation view
+            Console.WriteLine("Consultation Button Pressed");
+        }
+
+        private void EditCoordinator_Click(object sender, RoutedEventArgs e)
+        {
+            //DataGridTextColumn.IsReadOnly = true;
+            Console.WriteLine("Class Button Pressed");
+        }
+
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: add code to change/open class view
+            Console.WriteLine("Class Button Pressed");
+        }
+    }
+}
